494ffe8d0d5d19a4bf725e046a6e6f9e19b7c14a
MoryakSPb <31323043+MoryakSPb@users.noreply.github.com>
2023-07-21 11:25:29 +0300

    Исправлено присоединение к существующей экспедиции (2)

--- src/Client/Pages/ExpeditionPage.razor.cs
+++ src/Client/Pages/ExpeditionPage.razor.cs
@@ -138,7 +138,7 @@ public partial class ExpeditionPage : ComponentBase
     public async Task CreateJoin()
     {
         using HttpResponseMessage result = await HttpClient.PatchAsync(
-            $"api/Expedition/{Id}/leave",
+            $"api/Expedition/{Id}/join",
             new ByteArrayContent(Array.Empty<byte>()));
         LeaveJoinDocId = await result.Content.ReadFromJsonAsync<int>();
         StateHasChanged();
